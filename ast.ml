ebbe83c013797e1f4b30321f24da29f53eccabbc
arisPapadop <ap7415@ic.ac.uk>
2018-06-22 15:11:59 +0200

    Formatting changes, nothing important.

--- ast.ml
+++ ast.ml
@@ -14,29 +14,34 @@ let rec pretty_print : Format.formatter -> term -> unit = fun fmt t ->
   | BinOp (t1, op, t2) ->
       Format.fprintf fmt "(%a %s %a)" pretty_print t1 op pretty_print t2
 
-(* Initial term matcher *)
-let rec nested_match : term -> pattern -> bool = fun t p ->
+(* Main matching method. *)
+let rec term_match : term -> pattern -> bool = fun t p ->
   match t with
   | BinOp (t1, op, t2) -> begin
-     match p with
-      | BinOp (Any, p_op, Any) -> p_op = op
-      | BinOp (p1, p_op, Any)  -> p_op = op && nested_match t1 p1
-      | BinOp (Any, p_op, p2)  -> p_op = op && nested_match t2 p2
-      | BinOp (p1, p_op, p2)   -> p_op = op && nested_match t1 p1
-                                            && nested_match t2 p2
-      | Any -> true
-      | _ -> false
+    match p with
+    | BinOp (Any, p_op, Any) -> p_op = op
+    | BinOp (p1, p_op, Any)  -> p_op = op && term_match t1 p1
+    | BinOp (Any, p_op, p2)  -> p_op = op && term_match t2 p2
+    | BinOp (p1, p_op, p2) -> p_op = op && term_match t1 p1 && term_match t2 p2
+    | Any -> true
+    | _ -> false
   end
   | Var x -> p = Var x || p = Any
   | Lit i -> p = Any
-  | _  -> false (* Should not get here. *)
+  | _  -> false (* Should not get here. Terms cannot allowed to contain Any. *)
 
+
+(*
+ * Initial term matcher - Given a term and a pattern returns all the subterms
+ * that match the pattern. Note that the "Any" pattern matches all subterms
+ * not just Var's and Lit's
+ * *)
 let matching : term ->  pattern -> term list = fun t p ->
   let rec matching_aux : term -> term list -> term list = fun cur acc ->
     match cur with
     | BinOp (t1, op, t2) ->
       let rest = matching_aux t2 (matching_aux t1 acc) in
-      if nested_match cur p then cur :: rest else rest
+      if term_match cur p then cur :: rest else rest
     | Var x -> if p = Var x || p = Any then cur :: acc else acc
     | Lit i -> if p = Any then cur :: acc else acc
     | _ -> acc
