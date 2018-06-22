38f1896bb97f56b2f43d8935961fde977cf6dc81
arisPapadop <ap7415@ic.ac.uk>
2018-06-22 12:13:42 +0200

    Added more cases to the matcher.

--- ast.ml
+++ ast.ml
@@ -2,7 +2,7 @@ type term =
   | Lit   of int
   | Var   of string
   | BinOp of term * string * term
-  | Empty
+  | Any
 
 type pattern = term
 
@@ -10,27 +10,31 @@ let rec pretty_print : Format.formatter -> term -> unit = fun fmt t ->
   match t with
   | Lit i -> Format.fprintf fmt "%i" i
   | Var s -> Format.fprintf fmt "%s" s
-  | Empty -> Format.fprintf fmt "_"
+  | Any -> Format.fprintf fmt "_"
   | BinOp (t1, op, t2) ->
       Format.fprintf fmt "(%a %s %a)" pretty_print t1 op pretty_print t2
 
-(* Make term examples *)
-
+(* Initial term matcher *)
 let matching : term ->  pattern -> term list = fun t p ->
   let rec matching_aux : term -> term list -> term list = fun cur acc ->
     match cur with
     | BinOp (t1, op, t2) -> begin
-      let rest = List.append (matching_aux t1 acc) (matching_aux t2 acc) in
+      let rest = matching_aux t2 (matching_aux t1 acc) in
         match p with
-        | BinOp (Empty, p_op, Empty) ->
+        | BinOp (Any, p_op, Any) ->
             if op = p_op then cur :: rest else rest
-        | BinOp (p1, p_op, Empty) -> if p1 = t1 && op = p_op then
+        | BinOp (p1, p_op, Any) -> if p1 = t1 && op = p_op then
           cur :: rest else rest
-        | BinOp (Empty, p_op, p2) -> if p2 = t2 && op = p_op then
+        | BinOp (Any, p_op, p2) -> if p2 = t2 && op = p_op then
           cur :: rest else rest
+        | BinOp (p1, p_op, p2) -> if p1 = t1 && p2 = t2 && op = p_op then
+          cur :: rest else rest
+        | Any -> cur :: rest
         | _ -> rest
     end
-    | _  -> acc
+    | Var x -> if p = Var x || p = Any then cur :: acc else acc
+    | Lit i -> if p = Any then cur :: acc else acc
+    | _ -> acc
   in matching_aux t []
 
 
